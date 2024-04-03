6c1883e3794d976b2cbc19556e9fcdd7cbd57812
qznetsow <leproknight@gmail.com>
2024-04-03 10:26:17 +0000

    deploy clean

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -11,16 +11,13 @@ import { randomInt } from "crypto";
     let packageName = "GM_ClaimMaster.pkg";
     let owner = Address.parse("UQBi8I_It4oENAaQOS-XSj2JYRJgUUhFvr_daAdkwpo9ALJ2");
     let master = Address.parse("0QCJ81a9ELPIYJGHxavNe7HVhAx_iojnPev_jmT_2PEgEO7T");;
-    // let vaultAddress = Address.parse("EQDa4VOnTYlLvDJ0gZjNYm5PXfSmmtL6Vs6A_CZEtXCNICq_");
-    // let kingypooladdress = Address.parse("EQDsIxN6kTHNTzkW-KDAFoOd7uK8IV_qhw8wR5NkYH1Gh_SQ");
-
 
     let init = await ClaimMaster.init(master, master, BigInt(60*3));
 
     let address = contractAddress(0, init);
     let data = init.data.toBoc();
-    console.log(init.code.toBoc().toString('base64'));
-    return;
+    // console.log(init.code.toBoc().toString('base64'));
+    // return;
     let pkg = fs.readFileSync(path.resolve(__dirname, "output", packageName));
 
     // Prepareing
